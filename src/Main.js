6dbf3b5c7829e8569140d517b69d0fc6a64d50e1
abdelhak002 <darbeida.abdelhak@gmail.com>
2022-03-30 18:20:31 +0100

    initial configs

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="bg-white px-6 py-8 dark:bg-slate-900 dark:text-gray-100">
+      Start From Main Component
+    </div>
+  );
+};
+
+export default Main;
